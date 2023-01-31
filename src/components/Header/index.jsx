3acd2dd8fc683e2f56d3ea187ebbb3f96ac589c8
Diani2611 <diana.261111@gmail.com>
2023-01-30 21:31:35 -0500

    Creación header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+export default function Header() {
+    return (
+        <section className='profile-header'>
+            <div className='profile-img'>
+                <div className='profile-image'></div>
+            </div>
+            <div className='profile-name'>
+                    <h1>Diana Carolina Gonzalez</h1>
+                    <h2>Productora Multimedia/Frontend web</h2>
+                </div>
+        </section>
+    )
+}
