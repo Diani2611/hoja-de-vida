5c9e0b6237f34cbf2da782d294f2f3ae0f9178ed
Diani2611 <diana.261111@gmail.com>
2023-02-14 17:00:43 -0500

    Ajustes body

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -6,15 +6,16 @@ export default function Body () {
             <div className='aboutme-description'>
                 <h2 className='aboutme-title'>SOBRE MÍ</h2>
                 <div className='aboutme-text'>
-                <span>Productora multimedia.<br></br>   Con conocimientos en marketing y desarrollo frontend usando tecnologías cómo HTML, CSS, Javascript y bases de React.
-                <br></br>Con manejo de Adobe photoshop, illustrator, lightroom y figma. 
-                <br></br> Experiencia en creación de paginas web, manejo de redes sociales y procesos administrativos, manejo de siigo, excel intermedio.</span>
+                <span>Tecnóloga en Producción multimedia.<br></br>   Con experiencia en creación de paginas web, manejo de redes sociales, conocimientos en marketing y desarrollo frontend usando tecnologias cómo HTML, CSS, Java Script, bases de React y C#.
+                <br></br>Elaboración de piezas gráficas para redes sociales, manejo de Adobe photoshop, illustrator, lightroom y figma. Edición de audio y video, modelado 3D.
+                <br></br>Con capacidad de análisis, aprendizaje constante, mejoramiento continuo, iniciativa e innovación. </span>
+                <hr className='aboutme-line'></hr>
                 </div>
             </div>
             <div className='experience-description'>
                 <div className='experience-text'>
                     <h2> EXPERIENCIA LABORAL</h2>
-                    <span>Head Marketing / Administrador</span> <br/>
+                    <span className='experiencie-position'>Head Marketing / Administrador</span> <br/>
                     <span>Tuprop S.A.S.. | Mayo 2020 - Octubre 2022</span>
                     <ul className='experience-functions'>
                         <li className='experience-functions__items'>Gestión y diseño de redes sociales</li>
@@ -23,13 +24,13 @@ export default function Body () {
                         <li className='experience-functions__items'>Manejo de bases de datos</li>
                         <li className='experience-functions__items'>Procesos administrativos, manejo de personal</li>
                     </ul>
-                    <span>Desarrollador frontend freelance </span> <br/>
+                    <span className='experiencie-position'>Desarrollador frontend freelance </span> <br/>
                     <span> Grupo ARF S.A.S.| Diciembre 2021 - Junio 2022</span>
                     <ul className='experience-functions'>
                         <li className='experience-functions__items'>Desarrollo web</li>
                         <li className='experience-functions__items'>Creación e integración de animaciones</li>
                     </ul>
-
+                    <hr className='profile-line'></hr>
                 </div>
             </div>
         </section>  
